4bdcc6650f4ddc053907430c78dabdfd6a8c7830
luismarioamor <luismarioamorsanchez@gmail.com>
2024-04-14 21:51:15 -0500

    pokemon three

---
+++ pages/types.d.ts
@@ -0,0 +1,20 @@
+declare module "hostShell/card" {
+  import React from "react";
+  import { IPokemon, IPokemonSpecies } from "@/types";
+
+  interface IProps {
+    src: string;
+    bgColorCard: string;
+    pokemon: IPokemon;
+    pokemonSpecies: IPokemonSpecies;
+  }
+
+  const CardPokemon: React.FC<IProps>;
+  export default CardPokemon;
+}
+
+declare module "pokemonThree/pokemonThree" {
+  import React from "react";
+  const PokemonThree: React.FC<{}>;
+  export default PokemonThree;
+}
